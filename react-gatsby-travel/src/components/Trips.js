2cc1d9aeb770c563eabd579e06b317aef72be168
Vitor Bertti <berttivitor@gmail.com>
2021-03-16 19:21:39 -0300

    Created trips component

---
+++ react-gatsby-travel/src/components/Trips.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Trips = () => {
+   return (
+     <ProductsContainer>
+        <ProductsHeading>Heading</ProductsHeading>
+        <ProductWrapper>Wrapper</ProductWrapper>
+     </ProductsContainer>
+   );
+};
+
+export default Trips;
--- react-gatsby-travel/src/pages/index.js
+++ react-gatsby-travel/src/pages/index.js
@@ -3,11 +3,13 @@ import Hero from '../components/Hero'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Trips from '../components/Trips'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Hero />
+    <Trips />
   </Layout>
 )
 
