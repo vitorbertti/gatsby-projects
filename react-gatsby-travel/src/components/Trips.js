abe4cf9e6105e41890a08607aeb28a9dc46fde5c
Vitor Bertti <berttivitor@gmail.com>
2021-03-17 20:58:51 -0300

    Added trips.json and images

--- react-gatsby-travel/src/components/Trips.js
+++ react-gatsby-travel/src/components/Trips.js
@@ -11,3 +11,19 @@ const Trips = () => {
 };
 
 export default Trips;
+
+const ProductsContainer = styled.div`
+   min-height: 100vh;
+   padding: 5rem calc((100vw - 1300) /2);
+   background: red;
+   color: #fff;
+`;
+
+const ProductsHeading = styled.div`
+   font-size: clamp(1.2rem 5vw, 3rem);
+   text-align: center;
+   margin-bottom: 5rem;
+   color: #000;
+`;
+
+const ProductWrapper = styled.div``;
