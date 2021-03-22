ccb60403467ca8b9aa5fc2cc93fec2be1a082f07
Vitor Bertti <berttivitor@gmail.com>
2021-03-22 20:49:11 -0300

    Added testimonials styles

--- react-gatsby-travel/src/components/Testimonials.js
+++ react-gatsby-travel/src/components/Testimonials.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components';
 import Img from 'gatsby-image';
+import IoMdChechmarkCircleOutline from 'reac-icons/io';
+import {FaRegLightbulb} from 'react-icons/fa';
 
 const Testimonials = () => {
    return (
@@ -25,11 +27,80 @@ const Testimonials = () => {
                </Testimonial>
             </ColumnOne>
             <ColumnTwo>
-               image
+               <Images />
             </ColumnTwo>
          </ContentWrapper>
       </TestimonialsContainer>
    )
 }
 
-export default Testimonials
+export default Testimonials;
+
+const TestimonialsContainer = styled.div`
+   width: 100%;
+   background: #fcfcfc;
+   color: #000;
+   padding: 5rem calc((100vw - 1300px) /2);
+   height: 100%;
+`;
+
+const TopLine = styled.p`
+   color: #077bf1;
+   font-size: 1rem;
+   padding-left: 2rem;
+   margin-bottom: 0.75rem;
+`;
+
+const Description = styled.p`
+   text-align: start;
+   padding-left: 2rem;
+   margin-bottom: 4rem;
+   font-size: clamp(1.5rem, 5vw, 2rem);
+   font-weight: bold;
+`;
+
+const ContentWrapper = styled.div`
+   display: grid;
+   grid-template-columns: 1fr 1fr;
+   padding: 0 2rem;
+
+   @media screen and (max-width: 768px) {
+      grid-template-columns: 1fr;
+   }
+`;
+
+const ColumnOne = styled.div`
+   display: grid;
+   grid-template-columns: 1fr 1fr;
+`;
+
+const Testimonial = styled.div`
+   padding-top: 1rem;
+   padding-right: 2rem;
+
+   h3 {
+      margin-bottom: 1rem;
+      font-size: 1.5rem;
+      font-style: italic;
+   }
+
+   p {
+      color: #3b3b3b;
+   }
+`;
+
+const ColumnTwo = styled.div`
+   display: grid;
+   grid-template-columns: 1fr 1fr;
+   margin-top: 2rem;
+   grid-gap: 10px;
+
+   @media screen and (max-width: 500px) {
+      grid-template-columns: 1fr;
+   }
+`;
+
+const Images = styled(Img)`
+   border-radius: 10px;
+   height: 100%;
+`;
