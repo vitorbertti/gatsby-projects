2e02f53d2a5eec5ad091797f4907df509439be4e
Vitor Bertti <berttivitor@gmail.com>
2021-03-24 19:42:13 -0300

    Created stats component

---
+++ react-gatsby-travel/src/components/Stats.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Stats = () => {
+   return (
+      <StatsContainer>
+         <Heading>Why Choose Us?</Heading>
+         <Wrapper></Wrapper>
+      </StatsContainer>
+   )
+}
+
+export default Stats;
