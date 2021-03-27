8e7e6ae679227e36d15f14bf615a470e638287c1
Vitor Bertti <berttivitor@gmail.com>
2021-03-27 19:58:12 -0300

    Created email component

---
+++ react-gatsby-travel/src/components/Email.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import EmailBg from '../assets/images/toronto.jpg';
+
+const Email = () => {
+   return (
+      <h1>Email</h1>
+   );
+};
+
+export default Email;
